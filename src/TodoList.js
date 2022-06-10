914b3c1d81c7d4b7ad5e5597225d9e9b7035e2c0
baria aziz <bariaziz1996@gmail.com>
2022-06-10 12:35:08 +0100

    create a basic todo app

---
+++ src/TodoList.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+
+const testList = [
+  { text: "first todo item", checked: false },
+  { text: "second todo item", checked: false },
+];
+
+const TodoList = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [todoInputItem, setTodoInputItem] = useState("");
+
+  useEffect(() => {
+    setTodoList(testList);
+  }, []);
+
+  const onChangeInput = (e) => {
+    setTodoInputItem(e.target.value);
+  };
+
+  const onChangeCheckbox = (e) => {
+    let newTodoList = todoList;
+    if (e.target.checked === true) {
+      newTodoList = todoList.map((item, id) => {
+        return parseInt(id) === parseInt(e.target.id)
+          ? { ...item, checked: true }
+          : item;
+      });
+    } else if (e.target.checked === false) {
+      newTodoList = todoList.map((item, id) => {
+        return parseInt(id) === parseInt(e.target.id)
+          ? { ...item, checked: false }
+          : item;
+      });
+    }
+    setTodoList(newTodoList);
+  };
+
+  const addTodoItem = () => {
+    if (!todoInputItem?.trim()) return;
+    const newTodoList = [
+      {
+        text: todoInputItem,
+        checked: false,
+      },
+      ...todoList,
+    ];
+    setTodoList(newTodoList);
+    setTodoInputItem("");
+  };
+
+  const removeTodoItem = (deleteId) => {
+    const newTodoList = todoList.filter((item, id) => id !== deleteId);
+    setTodoList(newTodoList);
+  };
+
+  return (
+    <div className="todo-list">
+      {/* form to add items */}
+      <div className="todo-list-form">
+        <div className="form-item">
+          <input
+            placeholder="Add an item..."
+            onChange={onChangeInput}
+            value={todoInputItem}
+            className="form-input"
+            type="text"
+          />
+          <button className="form-button" onClick={addTodoItem}>
+            Add
+          </button>
+        </div>
+      </div>
+      {/* list of added items */}
+      <div className="todo-list-items">
+        {todoList.length > 0 ? (
+          todoList.map((item, id) => {
+            return (
+              <div className="todo-list-item" key={id}>
+                <input
+                  type="checkbox"
+                  onChange={onChangeCheckbox}
+                  id={id}
+                  checked={item.checked}
+                />
+                <span className={item.checked ? "checked" : ""}>
+                  {item.text}
+                </span>
+                <span
+                  className="delete-icon"
+                  onClick={() => {
+                    removeTodoItem(id);
+                  }}
+                >
+                  ❌
+                </span>
+              </div>
+            );
+          })
+        ) : (
+          <div className="todo-list-item">
+            <span>Theres nothing to do!</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
